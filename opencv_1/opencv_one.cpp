997c2a58e68e64dc1f6e4843dc32235fe9e99ae2
HuangLu <709485670@qq.com>
2016-01-16 20:07:38 +0800

    瞎搞

--- opencv_1/opencv_one.cpp
+++ opencv_1/opencv_one.cpp
@@ -19,6 +19,7 @@ int main()
 	const char *pstrWindowsToolBar = "Threshold";
 	///////////////////////////////////////////////
 	/////////////////////////////////////////////
+	//瞎搞搞
 	//从文件中载入图像的灰度图CV_LOAD_IMAGE_GRAYSCALE - 灰度图
 	g_pSrcImage = cvLoadImage(pstrImageName, CV_LOAD_IMAGE_GRAYSCALE);
 	g_pCannyImg = cvCreateImage(cvGetSize(g_pSrcImage), IPL_DEPTH_8U, 1);
